d5084504c0153e88fbf6980cb10dbeceded3d985
Leonardo Belilo <leonardoBelilo@outlook.com>
2023-03-14 18:22:12 -0300

    add typeorm and configure detabase postgres

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import {  CreateUserDto } from './dtos/createUser.dto';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-
+    constructor(private readonly userService: UserService) {
+    }
     @Post()
-    async createUser(@Body() createUser:CreateUserDto){
-        return {...createUser,
-        password:undefined}
+    async createUser(@Body() createUser: CreateUserDto) {
+
+        return this.userService.createUser(createUser)
+    }
+    @Get()
+    async getAllUser(): Promise<UserEntity[]> {
+        return this.userService.getAllUser()
     }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { hash } from 'bcrypt';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+
+@Injectable()
+export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) { }
+
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+        const saltOrRounds = 10;
+        const newPasswordHash = await hash(createUserDto.password, saltOrRounds);
+        return this.userRepository.save({ ...createUserDto, password: newPasswordHash })
+
+
+    }
+
+    async getAllUser(): Promise<UserEntity[]> {
+        return this.userRepository.find()
+    }
+}
